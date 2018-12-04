bdf629277d53842b65816f674872e3ee67cea7b3
premek <premek@users.noreply.github.com>
2018-12-05 00:50:26 +0100

    example usage with love2d

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # oscl
+
+- Only server for now
+- only strings and int params for now...
---
+++ main.lua
@@ -0,0 +1,26 @@
+-- example usage with love2d
+-- $ oscsend localhost 7773 /message/param i 255
+
+local Server = require("oscl.server")
+local server = Server("*", 7773)
+
+local s = 10
+local r = 0
+
+server.handlers[".*"] = print -- log everything
+server.handlers["/message"] = function() s=200 end
+server.handlers["/message/param.*"] = function(val) r=val end
+
+
+function love.draw(dt)
+    love.graphics.setColor(r/255, 0, 0)
+    love.graphics.circle("fill", 300, 300, s, 5)
+end
+
+function love.update(dt) 
+ server:update()
+ 
+ s=s-0.2
+end
+
+
